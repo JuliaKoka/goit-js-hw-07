ec8e5e7015025f2b8e25d78827f25ed25f462133
JuliaKoka <155240492+JuliaKoka@users.noreply.github.com>
2024-04-29 00:49:33 +0300

    task 1 corretion

--- js/task-1.js
+++ js/task-1.js
@@ -2,9 +2,9 @@ const categories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach((value) => {
-  const headingValue = value.querySelectorAll("h2");
-  const sublistQuantity = value.querySelectorAll("li");
+  const headingValue = value.querySelector("h2").textContent;
+  const sublistQuantity = value.querySelectorAll("li").length;
 
-  console.log(`Category: ${headingValue.textContent}`);
-  console.log(`Elements: ${sublistQuantity.length}`);
+  console.log(`Category: ${headingValue}`);
+  console.log(`Elements: ${sublistQuantity}`);
 });
