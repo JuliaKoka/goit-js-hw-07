723c3665fff5fa5bd0568b1b479961bc3cab409c
JuliaKoka <155240492+JuliaKoka@users.noreply.github.com>
2024-04-28 23:10:35 +0300

    Task 4 without css

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+
+  const formObject = {
+    email: elements.email.value,
+    password: elements.password.value,
+  };
+
+  if (elements.email.value === "" || elements.password.value === "") {
+    console.log("All form fields must be filled in");
+  } else {
+    console.log(formObject);
+  }
+}
